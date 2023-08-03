6654c63338a9f767d378622d80858886528e8717
jeremytooo <jeremy.espinoza.jr@unadeca.net>
2023-08-03 00:59:16 -0600

    commit

---
+++ src/pages/Picksbans3.js
@@ -0,0 +1,94 @@
+import "./Picksbans3.css";
+const Picksbans3 = () => {
+  return (
+    <div className="picksbans-3">
+      <div className="picksbans-3-child" />
+      <div className="picksbans-3-item" />
+      <img className="picksbans-3-inner" alt="" src="/line-3.svg" />
+      <img className="line-icon" alt="" src="/line-4.svg" />
+      <div className="top-red" />
+      <div className="jg-red" />
+      <div className="mid-red" />
+      <div className="adc-red" />
+      <div className="support-red" />
+      <div className="red-team">
+        <p className="red">RED</p>
+        <p className="red">{`TEAM `}</p>
+      </div>
+      <div className="line-div" />
+      <img className="image-4-icon" alt="" src="/image-4@2x.png" />
+      <img className="pngaaa-1-icon" alt="" src="/pngaaa-1@2x.png" />
+      <div className="rectangle-div" />
+      <div className="picksbans-3-child1" />
+      <div className="picksbans-3-child2" />
+      <div className="picksbans-3-child3" />
+      <img className="picksbans-3-child4" alt="" src="/line-3.svg" />
+      <img className="picksbans-3-child5" alt="" src="/line-4.svg" />
+      <div className="top-blue" />
+      <div className="picksbans-3-child6" />
+      <div className="picksbans-3-child7" />
+      <div className="picksbans-3-child8" />
+      <div className="jg-blue" />
+      <div className="mid-jg" />
+      <div className="adc-blue" />
+      <div className="support-blue" />
+      <div className="blue-team">
+        <p className="red">{`BLUE TEAM `}</p>
+      </div>
+      <div className="picksbans-3-child9" />
+      <img className="image-6-icon" alt="" src="/image-4@2x.png" />
+      <img className="pngaaa-2-icon" alt="" src="/pngaaa-1@2x.png" />
+      <div className="pick-0">PICK - 0</div>
+      <div className="ban-0">BAN - 0</div>
+      <div className="ban-01">BAN - 0</div>
+      <div className="ban-02">BAN - 0</div>
+      <div className="ban-03">BAN - 0</div>
+      <div className="ban-04">BAN - 0</div>
+      <div className="pick-01">PICK - 0</div>
+      <div className="pick-02">PICK - 0</div>
+      <div className="pick-03">PICK - 0</div>
+      <div className="pick-04">PICK - 0</div>
+      <div className="picksbans-3-child10" />
+      <div className="picksbans-3-child11" />
+      <div className="picksbans-3-child12" />
+      <div className="picksbans-3-child13" />
+      <div className="picksbans-3-child14" />
+      <div className="picksbans-3-child15" />
+      <div className="picksbans-3-child16" />
+      <div className="picksbans-3-child17" />
+      <div className="picksbans-3-child18" />
+      <div className="picksbans-3-child19" />
+      <div className="picksbans-3-child20" />
+      <div className="picksbans-3-child21" />
+      <img className="image-7-icon" alt="" src="/image-7@2x.png" />
+      <img className="image-8-icon" alt="" src="/image-7@2x.png" />
+      <div className="picksbans-3-child22" />
+      <div className="picksbans-3-child23" />
+      <div className="picksbans-3-child24" />
+      <div className="picksbans-3-child25" />
+      <div className="picksbans-3-child26" />
+      <div className="pick-05">PICK - 0</div>
+      <div className="ban-05">BAN - 0</div>
+      <div className="pick-06">PICK - 0</div>
+      <div className="ban-06">BAN - 0</div>
+      <div className="pick-07">PICK - 0</div>
+      <div className="pick-08">PICK - 0</div>
+      <div className="pick-09">PICK - 0</div>
+      <div className="ban-07">BAN - 0</div>
+      <div className="ban-08">BAN - 0</div>
+      <div className="ban-09">BAN - 0</div>
+      <img className="image-9-icon" alt="" src="/image-9@2x.png" />
+      <img className="image-10-icon" alt="" src="/image-9@2x.png" />
+      <img className="image-13-icon" alt="" src="/image-13@2x.png" />
+      <img className="image-14-icon" alt="" src="/image-13@2x.png" />
+      <img className="image-12-icon" alt="" src="/image-12@2x.png" />
+      <img className="image-11-icon" alt="" src="/image-12@2x.png" />
+      <button className="back">
+        <div className="back-child" />
+        <img className="back-item" alt="" src="/arrow-1.svg" />
+      </button>
+    </div>
+  );
+};
+
+export default Picksbans3;
